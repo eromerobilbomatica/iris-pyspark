1640ab0f2e53c697568d00309ec4d709ac48053a
eromerobilbomatica <eromero@bilbomatica.es>
2023-09-13 09:40:29 +0000

    configuración inicial spark en kedro

--- src/iris_pyspark/hooks.py
+++ src/iris_pyspark/hooks.py
@@ -1,7 +1,8 @@
 from kedro.framework.hooks import hook_impl
 from pyspark import SparkConf
 from pyspark.sql import SparkSession
-
+import os
+import subprocess
 
 class SparkHooks:
     @hook_impl
@@ -10,14 +11,21 @@ class SparkHooks:
         defined in project's conf folder.
         """
 
+        dst_lh_url  = os.environ.get('DST_LH_URL', 'spark://spark-master-0.spark-headless.demml.svc.cluster.local:7077')
+        dst_lh_appn = os.environ.get('DST_LH_APPN', 'iris_naive_bayes_classification_pyspark_EDU')
+        my_pod_ip = subprocess.run(['hostname', '-I'], stdout=subprocess.PIPE).stdout.decode('utf-8').strip(' \n\t')
+
         # Load the spark configuration in spark.yaml using the config loader
         parameters = context.config_loader["spark"]
         spark_conf = SparkConf().setAll(parameters.items())
 
         # Initialise the spark session
         spark_session_conf = (
-            SparkSession.builder.appName(context.project_path.name)
-            .enableHiveSupport()
+            SparkSession.builder \
+            .master(dst_lh_url) \
+            .appName(dst_lh_appn) \
+            .enableHiveSupport() \
+            .config('spark.driver.host', my_pod_ip) \
             .config(conf=spark_conf)
         )
         _spark_session = spark_session_conf.getOrCreate()
