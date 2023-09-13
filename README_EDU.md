4d6b5246b35692e2e67bc5d58dc841590836fe05
eromerobilbomatica <eromero@bilbomatica.es>
2023-09-13 12:57:59 +0000

    desintalado java openjdk version "1.8.0_362-362"

--- README_EDU.md
+++ README_EDU.md
@@ -26,10 +26,17 @@
 1. Instalalo las dependencias
     1. Incluyo pyspark==3.3.1 en el src/requirements.txt
     1. pip install -r src/requirements.txt
-1. Instalo java
+
+<!-- 1. Instalo java
     1. wget https://builds.openlogic.com/downloadJDK/openlogic-openjdk/8u362-b09/openlogic-openjdk-8u362-b09-linux-x64.tar.gz
     1. tar -xvzf openlogic-openjdk-8u362-b09-linux-x64.tar.gz
     1. sudo mv openlogic-openjdk-8u362-b09-linux-x64 /usr/lib/jvm/
     1. sudo update-alternatives --install /usr/bin/java java /usr/lib/jvm/bin/java 1
     1. sudo update-alternatives --config java
     1. para comprobar que la versión java coincide con el DEMML ``java -version``
+1. Desintalación de esta versión de java por completo
+    1. Eliminar el directorio Java: sudo rm -rf /usr/lib/jvm/openlogic-openjdk-8u362-b09-linux-x64
+    1. Eliminar los enlaces simbólicos de Java:
+        1. sudo rm -f /usr/bin/java
+        1. sudo rm -f /usr/lib/jvm/default-java
+    1. Eliminar las entradas de Java del menú de actualización de alternativas: sudo update-alternatives --remove-all java -->
--- src/iris_pyspark/hooks.py
+++ src/iris_pyspark/hooks.py
@@ -12,36 +12,32 @@ class SparkHooks:
         """
 
         dst_lh_url  = os.environ.get('DST_LH_URL', 'spark://spark-master-0.spark-headless.demml.svc.cluster.local:7077')
-        dst_lh_appn = os.environ.get('DST_LH_APPN', 'iris_naive_bayes_classification_pyspark_EDU')
+        dst_lh_appn = os.environ.get('DST_LH_APPN', 'iris_naive_bayes_classification_pyspark_EDU_4')
         my_pod_ip = subprocess.run(['hostname', '-I'], stdout=subprocess.PIPE).stdout.decode('utf-8').strip(' \n\t')
 
-        # Initialise the spark session
-        spark_session_conf = (
-            SparkSession.builder 
-                .master(dst_lh_url)
-                .appName(dst_lh_appn)
-                .config('spark.jars.packages','org.apache.iceberg:iceberg-spark-runtime-3.3_2.12:1.1.0,org.mariadb.jdbc:mariadb-java-client:2.3.0,org.postgresql:postgresql:42.5.4,software.amazon.awssdk:bundle:2.17.257,software.amazon.awssdk:url-connection-client:2.17.257,software.amazon.awssdk:s3:2.17.257,software.amazon.awssdk:iam:2.17.257,org.apache.hadoop:hadoop-aws:3.2.3') 
-                .config('spark.sql.extensions','org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions') 
-                .config('spark.sql.catalog.spark_catalog','org.apache.iceberg.spark.SparkSessionCatalog') 
-                .config('spark.sql.catalog.spark_catalog.catalog-impl','org.apache.iceberg.jdbc.JdbcCatalog') 
-                .config('spark.sql.catalog.spark_catalog.uri','jdbc:postgresql://postgresql:5432/iceberg') 
-                .config('spark.driver.host', my_pod_ip) 
-                .config('spark.driver.maxResultSize', '3g') 
-                .config('spark.scheduler.mode', 'FAIR')  
-                .config('spark.sql.catalog.spark_catalog.jdbc.useSSL','false')
-                .config('spark.sql.catalog.spark_catalog.jdbc.user','iceberg') 
-                .config('spark.sql.catalog.spark_catalog.jdbc.password','iceberg')
-                .config('spark.sql.catalog.spark_catalog.warehouse','s3a://warehouse/')
-                .config('spark.sql.catalog.spark_catalog.s3.endpoint','http://minio:9000')
-                .config('spark.sql.catalog.spark_catalog.io-impl', 'org.apache.iceberg.aws.s3.S3FileIO')
-                .config('spark.sql.defaultCatalog','spark_catalog')
-                .config('spark.hadoop.fs.s3a.endpoint','http://minio:9000')
-                .config('spark.hadoop.fs.s3a.access.key','admin')
-                .config('spark.hadoop.fs.s3a.secret.key','t4bl4red0nd4')
-                .config('spark.hadoop.fs.s3a.path.style.access','true')
-                .config('spark.hadoop.fs.s3a.impl','org.apache.hadoop.fs.s3a.S3AFileSystem')
-                .config("spark.hadoop.fs.s3a.connection.ssl.enabled", "false")
-                .config("spark.hadoop.fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider")
-        )
-        _spark_session = spark_session_conf.getOrCreate()
-        _spark_session.sparkContext.setLogLevel("WARN")
+        spark = SparkSession.builder \
+            .master(dst_lh_url) \
+            .appName(dst_lh_appn) \
+            .config('spark.jars.packages','org.apache.iceberg:iceberg-spark-runtime-3.3_2.12:1.1.0,org.mariadb.jdbc:mariadb-java-client:2.3.0,org.postgresql:postgresql:42.5.4,software.amazon.awssdk:bundle:2.17.257,software.amazon.awssdk:url-connection-client:2.17.257,software.amazon.awssdk:s3:2.17.257,software.amazon.awssdk:iam:2.17.257,org.apache.hadoop:hadoop-aws:3.2.3') \
+            .config('spark.driver.host', my_pod_ip) \
+            .getOrCreate()
+            # .config('spark.sql.extensions','org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions') \
+            # .config('spark.sql.catalog.spark_catalog','org.apache.iceberg.spark.SparkSessionCatalog') \
+            # .config('spark.sql.catalog.spark_catalog.catalog-impl','org.apache.iceberg.jdbc.JdbcCatalog') \
+            # .config('spark.sql.catalog.spark_catalog.uri','jdbc:postgresql://postgresql:5432/iceberg') \
+            # .config('spark.sql.catalog.spark_catalog.jdbc.useSSL','false') \
+            # .config('spark.sql.catalog.spark_catalog.jdbc.user','iceberg') \
+            # .config('spark.sql.catalog.spark_catalog.jdbc.password','iceberg') \
+            # .config('spark.sql.catalog.spark_catalog.warehouse','s3a://warehouse/') \
+            # .config('spark.sql.catalog.spark_catalog.s3.endpoint','http://minio:9000') \
+            # .config('spark.sql.catalog.spark_catalog.io-impl', 'org.apache.iceberg.aws.s3.S3FileIO') \
+            # .config('spark.sql.defaultCatalog','spark_catalog') \
+            # .config('spark.hadoop.fs.s3a.endpoint','http://minio:9000') \
+            # .config('spark.hadoop.fs.s3a.access.key','admin') \
+            # .config('spark.hadoop.fs.s3a.secret.key','t4bl4red0nd4') \
+            # .config('spark.hadoop.fs.s3a.path.style.access','true') \
+            # .config('spark.hadoop.fs.s3a.impl','org.apache.hadoop.fs.s3a.S3AFileSystem') \
+            # .config("spark.hadoop.fs.s3a.connection.ssl.enabled", "false") \
+            # .config("spark.hadoop.fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider") \
+            # .getOrCreate()
+        spark.sparkContext.setLogLevel("WARN")
