f1479dc18f1ddcb13876f1d01a9001e99d80357c
eromerobilbomatica <eromero@bilbomatica.es>
2023-09-19 10:38:38 +0000

    primer pipeline completo en kedro

--- src/iris_pyspark/pipelines/data_science/nodes.py
+++ src/iris_pyspark/pipelines/data_science/nodes.py
@@ -2,3 +2,72 @@
 This is a boilerplate pipeline 'data_science'
 generated using Kedro 0.18.13
 """
+
+from pyspark.ml.classification import NaiveBayes
+from pyspark.ml.evaluation import MulticlassClassificationEvaluator
+from pyspark.ml.feature import VectorAssembler
+from pyspark.sql import DataFrame
+from typing import Tuple, Dict
+
+def transform_data(iris: DataFrame) -> DataFrame:
+    """
+    En este nodo, realiza la transformación de datos 
+    utilizando VectorAssembler.
+    """
+
+    feature_assembler = VectorAssembler(
+        inputCols=['sepal_length', 'sepal_width', 'petal_length', 'petal_width'],
+        outputCol='Features'
+    )
+    output = feature_assembler.transform(iris)
+
+    return output.select('Features', 'target')
+
+
+def split_data(transformed_data: DataFrame) -> Tuple[DataFrame, DataFrame]:
+    """
+    En este nodo, divide los datos en conjuntos de entrenamiento y prueba.
+    """
+
+    train_data, test_data = transformed_data.randomSplit([0.8, 0.2])
+
+    return train_data, test_data
+
+
+def train_naive_bayes(train_data: DataFrame) -> NaiveBayes:
+    """
+    En este nodo, ajusta el modelo Naive Bayes a los datos de entrenamiento.
+    """
+
+    nb = NaiveBayes(featuresCol='Features', labelCol='target')
+    nb_model = nb.fit(train_data)
+
+    return nb_model
+
+def predict_and_evaluate(nb_model: NaiveBayes, test_data) -> Dict:
+    """
+    En este nodo, realiza la predicción en el conjunto de prueba y calcula métricas de evaluación.
+    """
+
+    y_pred = nb_model.transform(test_data)
+
+    evaluator = MulticlassClassificationEvaluator(
+        labelCol='target', predictionCol='prediction'
+    )
+
+    accuracy = evaluator.evaluate(y_pred)
+    precision = evaluator.evaluate(y_pred, {evaluator.metricName: 'weightedPrecision'})
+    recall = evaluator.evaluate(y_pred, {evaluator.metricName: 'weightedRecall'})
+    f1_score = evaluator.evaluate(y_pred, {evaluator.metricName: 'f1'})
+
+    print("- Accuracy: ", accuracy)
+    print("- Precision: ", precision)
+    print("- Recall: ", recall)
+    print("- F1 score: ", f1_score)
+
+    return {
+        "accuracy": accuracy,
+        "precision": precision,
+        "recall": recall,
+        "f1_score": f1_score,
+    }
--- src/iris_pyspark/pipelines/data_science/pipeline.py
+++ src/iris_pyspark/pipelines/data_science/pipeline.py
@@ -3,8 +3,44 @@ This is a boilerplate pipeline 'data_science'
 generated using Kedro 0.18.13
 """
 
-from kedro.pipeline import Pipeline, pipeline
+from kedro.pipeline import Pipeline, pipeline, node
+
+from .nodes import (
+    transform_data, split_data, 
+    train_naive_bayes, predict_and_evaluate
+)
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+    return pipeline(
+        [
+            node(
+                func = transform_data, 
+                inputs = "iris_iceberg_table",
+                outputs = "transformed_data",
+                name="VectorAssembler",
+                tags="data_science"
+            ),
+            node(
+                func = split_data, 
+                inputs = "transformed_data",
+                outputs = ["train_data", "test_data"],
+                name="split_data",
+                tags="data_science"
+            ),
+            node(
+                func = train_naive_bayes, 
+                inputs = "train_data",
+                outputs = "naiveBayes_model",
+                name="train_naive_bayes",
+                tags="data_science"
+            ),
+            node(
+                func = predict_and_evaluate, 
+                inputs = ["naiveBayes_model", "test_data"],
+                outputs = "evaluation_results",
+                name="predict_and_evaluate",
+                tags="data_science"
+            ),
+        ]
+    )
